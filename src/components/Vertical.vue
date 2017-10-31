75b1e7b2a50d179dabc768f67320dfbbaae8b643
feke9432 <33196340+feke9432@users.noreply.github.com>
2017-10-31 18:41:01 +0800

    Add files via upload
    第一次上传

---
+++ src/components/Vertical.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="vertical">
+		<h1>{{ title }}</h1>
+		<section class="one">
+			<p>1.</p>
+		</section>
+	</div>
+</template>
+
+<script>
+	export default {
+		data () {
+			return {
+				title: '垂直居中模块'
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	
+</style>
